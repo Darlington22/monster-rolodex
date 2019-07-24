ec88027b87e1c776f3dc9b59e9869d5cac1c45cf
Otchere Darlington <otcheredarlington@gmail.com>
2019-07-24 21:31:51 +0000

    Updates

---
+++ precache-manifest.ae5d06e6b12351f21cf8490250173c58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c219751e836d4355518c1e31d29f115",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "02d7e8eac2cec6e54b9f",
+    "url": "/monster-rolodex/static/css/main.554f9733.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "02d7e8eac2cec6e54b9f",
+    "url": "/monster-rolodex/static/js/main.371b3510.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
